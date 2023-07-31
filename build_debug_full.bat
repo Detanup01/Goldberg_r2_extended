c346bf5d39fed4dfd63d0a0581846dd3ddcc0ea0
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2023-07-31 21:53:09 +0200

    hehe, last

--- build_debug_full.bat
+++ build_debug_full.bat
@@ -1,6 +1,6 @@
 del /Q /S debug_full\*
-rmdir /S /Q full
-mkdir full
+rmdir /S /Q debug_full
+mkdir debug_full
 call build_env_x86.bat
 cl /std:c++17 /LD /DNDEBUG emu.cpp User32.lib Shell32.lib /EHsc /MP12 /Ox /link /OUT:debug_full\upc_r2_loader.dll
 echo.
