8625f099558d17eaf00ef909536ba78c80d93fb1
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2023-07-31 21:57:37 +0200

    i swear

--- build_full.bat
+++ build_full.bat
@@ -5,7 +5,7 @@ call build_env_x86.bat
 cl /std:c++17 /LD /DEMU_RELEASE_BUILD /DNDEBUG emu.cpp User32.lib Shell32.lib /EHsc /MP12 /Ox /link /OUT:full\upc_r2_loader.dll
 echo.
 call build_env_x64.bat
-cl /std:c++17 /LD /DEMU_RELEASE_BUILD /DNDEBUG emu.cpp User32.lib Shell32.lib /EHsc /MP12 /Ox /link /OUT:full\uplay_r2_loader64.dll
+cl /std:c++17 /LD /DEMU_RELEASE_BUILD /DNDEBUG emu.cpp User32.lib Shell32.lib /EHsc /MP12 /Ox /link /OUT:full\upc_r2_loader64.dll
 echo Built
 pause
 echo Done
