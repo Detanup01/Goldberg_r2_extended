ac345604cf411d0b895571c629338157c3ea30e0
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2024-11-13 12:49:14 +0100

    Remove upc.h from build (still building with but no need to declare the headers)

--- build_debug_x64.bat
+++ build_debug_x64.bat
@@ -2,7 +2,7 @@ del /Q /S debug64\*
 rmdir /S /Q debug64
 mkdir debug64
 call build_env_x64.bat
-cl /std:c++20 /LD /DNDEBUG emu.cpp upc.h User32.lib Shell32.lib Ole32.lib /EHsc /MP12 /Ox /link /OUT:debug64\upc_r2_loader64.dll
+cl /std:c++20 /LD /DNDEBUG emu.cpp User32.lib Shell32.lib Ole32.lib /EHsc /MP12 /Ox /link /OUT:debug64\upc_r2_loader64.dll
 echo Built
 pause
 echo Done
--- build_x86.bat
+++ build_x86.bat
@@ -2,7 +2,7 @@ del /Q /S release86\*
 rmdir /S /Q release86
 mkdir release86
 call build_env_x86.bat
-cl /std:c++20 /LD /DEMU_RELEASE_BUILD /DNDEBUG emu.cpp upc.h User32.lib Shell32.lib Ole32.lib /EHsc /MP12 /Ox /link /OUT:release86\upc_r2_loader.dll
+cl /std:c++20 /LD /DEMU_RELEASE_BUILD /DNDEBUG emu.cpp User32.lib Shell32.lib Ole32.lib /EHsc /MP12 /Ox /link /OUT:release86\upc_r2_loader.dll
 echo Built
 pause
 echo Done
